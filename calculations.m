05c1257a3e3fa1648482f6b22c996293e2762b3b
adithyahosapate <adihosapate@gmail.com>
2017-09-18 20:11:06 +0530

    added calculations

---
+++ calculations.m
@@ -0,0 +1,14 @@
+% A short introductory script for Basic current and voltage calculations
+
+%EDIT THIS LATER
+
+%*******Design Choices****************
+capcitance=
+n_capacitors=
+rail_length=
+rail_resistance=
+Injection_velocity=
+Magnetic_field=
+ %************************************
+
+
