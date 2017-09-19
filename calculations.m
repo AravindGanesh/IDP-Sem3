9739de107d62076b4a0a2afcb6d78bca47778aac
adithyahosapate <adihosapate@gmail.com>
2017-09-19 23:54:18 +0530

    changed calculations

--- calculations.m
+++ calculations.m
@@ -2,13 +2,26 @@
 
 %EDIT THIS LATER
 
-%*******Design Choices****************
-capcitance=
-n_capacitors=
-rail_length=
-rail_resistance=
-Injection_velocity=
-Magnetic_field=
- %************************************
+ %*****Charging the capacitor bank******** 
+capcitance=0.1
+Voltage=200
+n_capacitors=10
+capacitor_rating=100
+Series_resistance=1000
+
+eff_capacitance=capcitance*n_capacitors
+Time_constant=eff_capacitance*Series_resistance
+charging_time=log(10)*Series_resistance*capcitance
+Energy_stored=0.5*eff_capacitance*Voltage^2
+%******************************************
+
+% *****************Firing the gun***************
+
+
+
+% *******TODO***********
+
+
+
 
 
