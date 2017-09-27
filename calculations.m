e3558b99bf96373293b3fd46a29da5159e3423ca
Aravindganesh <ee16btech11026>
2017-09-27 12:50:39 +0530

    Consider More parameters and made calculations

--- calculations.m
+++ calculations.m
@@ -1,18 +1,30 @@
 % A short introductory script for Basic current and voltage calculations
 
 %EDIT THIS LATER
-
+% This shit is ideal
  %*****Charging the capacitor bank******** 
-capcitance=0.1
-Voltage=200
+capcitance=1e-3
+Voltage=100
 n_capacitors=10
 capacitor_rating=100
 Series_resistance=1000
-
+mass = 10e-3
+mu0 = 4*pi*1e-7
+radius = 5e-2
+turns = 20
+projectile_resistance = 1
+t=linspace(0,10,1000);
+coil_current = 10
+rail_current = 
+B_coil = mu0*turns*current/(2*radius)
 eff_capacitance=capcitance*n_capacitors
+
+B_rails = 
 Time_constant=eff_capacitance*Series_resistance
 charging_time=log(10)*Series_resistance*capcitance
-Energy_stored=0.5*eff_capacitance*Voltage^2
+Energy_stored=0.5*eff_capacitance*(Voltage*0.9)^2
+%ideal_velocity = sqrt(2*Energy_stored/mass)
+
 %******************************************
 
 % *****************Firing the gun***************
